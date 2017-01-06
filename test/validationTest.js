a1ac2adde1787c0cdb69eb3209b645103bf71bcd
Chetan Sanghani <crsanghani@gmail.com>
2017-01-06 16:21:22 +0000

    returns -1 if no valid strings are tests

--- test/validationTest.js
+++ test/validationTest.js
@@ -7,4 +7,10 @@ describe("Validation", function () {
   it("should return -1 by default", function () {
     expect(validateLength()).to.equal(-1);
   });
+
+  it("should return -1 if no valid strings are found", function () {
+    var string = 'as234as';
+
+    expect(validateLength(string)).to.equal(-1);
+  });
 });
