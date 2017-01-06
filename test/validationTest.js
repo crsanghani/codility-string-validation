509a2bf3a250b556d184426bdefc84dea17701ad
Chetan Sanghani <crsanghani@gmail.com>
2017-01-06 19:08:12 +0000

    refactored code to be dry

--- test/validationTest.js
+++ test/validationTest.js
@@ -1,16 +1,22 @@
 var chai = require('chai');
 var expect = chai.expect;
 var validation = require('../src/validation.js');
-var validateLength = validation.validateLength;
+var string;
+var validateLength = expect(validation.validateLength(string));
+
 
 describe("Validation", function () {
   it("should return -1 by default", function () {
-    expect(validateLength()).to.equal(-1);
+    validateLength.to.equal(-1);
   });
 
   it("should return -1 if no valid strings are found", function () {
     var string = 'as234as';
+    validateLength.to.equal(-1);
+  });
 
-    expect(validateLength(string)).to.equal(-1);
+  it("should return 2 if input is 'a9091Ba'", function () {
+    var string = 'a9091BA';
+    validateLength.to.equal(2);
   });
 });
